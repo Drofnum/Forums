c2fd8f9f113ef5e667bd97e24d0e76aa9c26dc0d
Drofnum <echaosaj@gmail.com>
2019-06-28 10:12:16 -0600

    First Commit

---
+++ Forums.Data/ForumsDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Forums.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forums.Data
+{
+    public class ForumsDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ForumsDbContext(DbContextOptions<ForumsDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+    }
+}
---
+++ Forums/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using Forums.Data;
+using Forums.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Forums.Areas.Identity.IdentityHostingStartup))]
+namespace Forums.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ForumsDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ForumsContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>()
+                    .AddEntityFrameworkStores<ForumsDbContext>();
+            });
+        }
+    }
+}
